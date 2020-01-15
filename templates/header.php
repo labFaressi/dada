1adeeaa1f99538b2d295a910325cce398252fbee
labFaressi <59740981+labFaressi@users.noreply.github.com>
2020-01-15 20:15:19 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -14,9 +14,9 @@
 <body class="grey lighten-4">
 	<nav class="white z-depth-0">
     <div class="container">
-      <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+      <a href="#" class="brand-logo brand-text">LuLu</a>
       <ul id="nav-mobile" class="right hide-on-small-and-down">
-        <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+        <li><a href="#" class="btn brand z-depth-0">Add Whatever You Wanted</a></li>
       </ul>
     </div>
   </nav>
