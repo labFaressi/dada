826566538b17fdc41cb5eb3cd638ce98596044ce
labFaressi <59740981+labFaressi@users.noreply.github.com>
2020-01-15 20:16:22 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -16,7 +16,7 @@
     <div class="container">
       <a href="#" class="brand-logo brand-text">LuLu</a>
       <ul id="nav-mobile" class="right hide-on-small-and-down">
-        <li><a href="#" class="btn brand z-depth-0">Add Whatever You Wanted</a></li>
+        <li><a href="#" class="btn brand z-depth-0">Add Whatever You Want</a></li>
       </ul>
     </div>
   </nav>
