117af6e86e681937fabf62c141ea4d6122ef7646
labFaressi <59740981+labFaressi@users.noreply.github.com>
2020-01-18 18:01:33 +0300

    Update add.php

--- add.php
+++ add.php
@@ -1,37 +1,50 @@
 <?php 
+
+	$email = $title = $ingredients = '';
+	$errors = array('email' => '', 'title' => '', 'ingredients' => '');
+
 	if(isset($_POST['submit'])){
-		$email = $title = $ingredients = '';
 		
 		// check email
 		if(empty($_POST['email'])){
-			echo 'Email adresi gerekli <br />';
+			$errors['email'] = 'An email is required';
 		} else{
 			$email = $_POST['email'];
 			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-				echo 'Email adresi uygun değil.';
+				$errors['email'] = 'Email must be a valid email address';
 			}
 		}
+
 		// check title
 		if(empty($_POST['title'])){
-			echo 'Ürün Adı Gerekli <br />';
+			$errors['title'] = 'A title is required';
 		} else{
 			$title = $_POST['title'];
 			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-				echo 'Ürün adında boşluk ve harfler dışında birşey kullanamazsınız.';
+				$errors['title'] = 'Title must be letters and spaces only';
 			}
 		}
+
 		// check ingredients
 		if(empty($_POST['ingredients'])){
-			echo 'En az 1 adet açıklayıcı bilgi gerekli <br />';
+			$errors['ingredients'] = 'At least one ingredient is required';
 		} else{
 			$ingredients = $_POST['ingredients'];
 			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
-				echo 'Ürün özelliklerini aralara ","(virgül) koyarak ayırınız.';
+				$errors['ingredients'] = 'Ingredients must be a comma separated list';
 			}
 		}
-	} // end POST check
-?>
 
+		if(array_filter($errors)){
+			//echo 'errors in form';
+		} else {
+			//echo 'form is valid';
+			header('Location: index.php');
+		}
+
+	} // end POST check
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -41,12 +54,15 @@
 	<section class="container grey-text">
 		<h4 class="center">Add a Pizza</h4>
 		<form class="white" action="add.php" method="POST">
-			<label>Email</label>
-			<input type="text" name="email">
-			<label>Ürün Adı</label>
-			<input type="text" name="title">
-			<label>Ürün İçeriği</label>
-			<input type="text" name="ingredients">
+			<label>Your Email</label>
+			<input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+			<div class="red-text"><?php echo $errors['email']; ?></div>
+			<label>Pizza Title</label>
+			<input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+			<div class="red-text"><?php echo $errors['title']; ?></div>
+			<label>Ingredients (comma separated)</label>
+			<input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+			<div class="red-text"><?php echo $errors['ingredients']; ?></div>
 			<div class="center">
 				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
